4ec89ca1ababf7e4e3cf54ab644ba3e196af51f6
Oliver Zott <zott_oliver@web.de>
2021-05-26 19:33:09 +0200

    Decorator example added

---
+++ OOP/decorators.py
@@ -0,0 +1,38 @@
+class Employee:
+
+    def __init__(self, first, last) -> None:
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return '{}.{}@mail.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return self.first + ' ' + self.last
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete name.')
+        self.first = None
+        self.last = None
+
+
+if __name__ == "__main__":
+
+    emp_1 = Employee('John', 'Smith')
+    emp_1.first = 'Jim'
+    emp_1.fullname = 'Olli Zott'
+
+    print(emp_1.first)
+    print(emp_1.email)
+    print(emp_1.fullname)
+
+    del emp_1.fullname
