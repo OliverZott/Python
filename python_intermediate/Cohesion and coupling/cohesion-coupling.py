2be092d03e20626e8baa07de3d88b9f755701269
Oliver Zott <zott_oliver@web.de>
2021-07-15 18:12:41 +0200

    Optimize code by decoupling and increasing cohesion

--- python_intermediate/Cohesion and coupling/cohesion-coupling.py
+++ python_intermediate/Cohesion and coupling/cohesion-coupling.py
@@ -2,52 +2,81 @@ import random
 import string
 
 
+class VehicleInfo:
+    brand: str
+    catalogue_price: int
+    electric: bool
+
+    def __init__(self, brand: str, catalogue_price: int, electric: bool) -> None:
+        self.brand = brand
+        self.catalogue_price = catalogue_price
+        self.electric = electric
+        self.tax = self.calc_tax()
+
+    def calc_tax(self) -> int:
+        tax_percentage = 0.05
+        if self.brand == "Tesla Model 3" or self.brand == "Volkswagen ID3":
+            tax_percentage = 0.02
+
+        return tax_percentage * self.catalogue_price
+
+
+class Vehicle:
+
+    def __init__(self, id: str, license_plate: str, info: object) -> None:
+        self.id = id
+        self.license_plate = license_plate
+        self.info = info
+
+    def __repr__(self):
+        return super().__repr__() + f"\nVehicle:   {self.info.brand}\n"
+
+
 class VehicleRegistry:
+    vehicle_info = {}
+
+    def add_vehicle_info(self, brand: str, electric: bool, catalogue_price: int) -> None:
+        self.vehicle_info[brand] = VehicleInfo(
+            brand=brand,
+            electric=electric,
+            catalogue_price=catalogue_price)
+
+    def __init__(self) -> None:
+        self.add_vehicle_info("Tesla Model 3", True, 60000)
+        self.add_vehicle_info("Volkswagen ID3", True, 35000)
+        self.add_vehicle_info("BMW 5", False, 45000)
 
     def generate_vehicle_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
 
     def generate_vehicle_license(self, id):
-        return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
+        return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))} "
+
+    # @staticmethod   not possible without instantiating, because "vehicle_info" is created on instatiation
+    def create_vehicle(self, brand: str):
+        vehicle_id = self.generate_vehicle_id(12)
+        license_plate = self.generate_vehicle_license(vehicle_id)
+        vehicle = Vehicle(vehicle_id, license_plate, self.vehicle_info[brand])
+        return vehicle
 
 
 class Application:
 
     def register_vehicle(self, brand: string):
-        # create a registry instance
-        registry = VehicleRegistry()
 
-        # generate a vehicle id of length 12
-        vehicle_id = registry.generate_vehicle_id(12)
+        vehicle_registry = VehicleRegistry()
+        vehicle = vehicle_registry.create_vehicle(brand)
+        print(vehicle)
 
-        # now generate license id for the vehicle
-        # using the first two characters of the vehilce id
-        license_plate = registry.generate_vehicle_license(vehicle_id)
-
-        # compute catalogue price
-        catalogue_price = 0
-        if brand == "Tesla Model 3":
-            catalogue_price = 60000
-        elif brand == "Volkswagen ID3":
-            catalogue_price = 35000
-        elif brand == "BMW 5":
-            catalogue_price = 45000
-
-        # compute the tax percentar (5% standardt , electric 2%)
-        tax_percentage = 0.05
-        if brand == "Tesla Model 3" or brand == "Volkswagen ID3":
-            tax_percentage = 0.02
-
-        tax = tax_percentage * catalogue_price
-
-        # print vehicle registration information
-        print("Registration compelte: Vehicle information:")
+        print("Registration complete: Vehicle information:")
         print(f"Brand: {brand}")
-        print(f"Id: {vehicle_id}")
-        print(f"License plate: {license_plate}")
-        print(f"Taxes: {tax}")
+        print(f"Id: {vehicle.id}")
+        # print(f"Id: {vehicle[info][vehicle_id]}")
+        print(f"License plate: {vehicle.license_plate}")
+        print(f"Brand: {vehicle.info.brand}")
+        print(f"Taxes: {vehicle.info.tax}")
 
 
-app = Application()
-
-app.register_vehicle("Tesla Model 3")
+if __name__ == "__main__":
+    app = Application()
+    app.register_vehicle("BMW 5")
--- python_intermediate/README.md
+++ python_intermediate/README.md
@@ -1,6 +1,6 @@
 # Resources
-
 https://www.youtube.com/watch?v=eiDyK_ofPPM&list=PLC0nd42SBTaNuP4iB4L6SJlMaHE71FG6N
+
 
 
 
@@ -8,19 +8,20 @@ https://www.youtube.com/watch?v=eiDyK_ofPPM&list=PLC0nd42SBTaNuP4iB4L6SJlMaHE71F
 # Cohesion and coupling
 https://www.youtube.com/watch?v=eiDyK_ofPPM
 
-- 
-
-
-
-
-
+- Look where the information is stored and decide how to split up 
 
 
 
 
 ------------------------------------------------------------------
-# Various
+# Cohesion and coupling
+https://www.youtube.com/watch?v=eiDyK_ofPPM
 
+
+ 
+
+------------------------------------------------------------------
+# Various
 ## Code Smells
 
 https://www.amazon.com/gp/product/0134757599/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0134757599&linkCode=as2&tag=johnovo-20&linkId=76277cc89642d31d242d99b0344cf89e
