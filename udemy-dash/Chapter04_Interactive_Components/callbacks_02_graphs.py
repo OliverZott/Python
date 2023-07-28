6a6577e8b6ce699233d92392eaa923d23886287b
Oliver Zott <zott_oliver@web.de>
2023-07-28 12:58:14 +0200

    Dash - interactive graphs

--- udemy-dash/Chapter04_Interactive_Components/callbacks_02_graphs.py
+++ udemy-dash/Chapter04_Interactive_Components/callbacks_02_graphs.py
@@ -7,9 +7,65 @@ import dash
 from dash import dcc  # type: ignore[attr-defined]
 from dash import html
 
-df = pd.read_csv("../data/gapminderDataFiveYear.csv")
+df = pd.read_csv("udemy-dash/data/gapminderDataFiveYear.csv")
+print(df.head())
+
+
+year_options = []
+for year in df["year"].unique():
+    year_options.append({"label": str(year), "value": year})
 
 app = dash.Dash(title=__name__)
+
+app.layout = html.Div(
+    [
+        dcc.Graph(
+            id="graph-gapminder",
+        ),
+        dcc.Dropdown(
+            id="year-dropdown",
+            options=year_options,
+            # value=df["year"].min(),
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output(component_id="graph-gapminder", component_property="figure"),
+    Input(component_id="year-dropdown", component_property="value"),
+)
+def update_graph(selected_year: dcc.Input):
+    df_by_year = df[df["year"] == selected_year]
+
+    traces = []
+
+    for continent_name in df_by_year["continent"].unique():
+        df_by_continent = df_by_year[df_by_year["continent"] == continent_name]
+
+        traces.append(
+            go.Scatter(
+                x=df_by_continent["gdpPercap"],
+                y=df_by_continent["lifeExp"],
+                text=df_by_continent["country"],
+                mode="markers",
+                marker=dict(size=12),
+                opacity=0.8,
+                name=continent_name,
+            )
+        )
+
+    return {
+        "data": traces,
+        "layout": go.Layout(
+            title="My scatter plot 2",
+            xaxis={
+                "title": "GDP Per Cap",
+                "type": "log",
+            },
+            yaxis={"title": "Life Expectancy"},
+        ),
+    }
 
 
 if __name__ == "__main__":
